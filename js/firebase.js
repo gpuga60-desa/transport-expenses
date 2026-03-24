a29ed465bf228642df75598e0e6e9716184791b6
gpuga60-desa <gpuga60@gmail.com>
2026-03-24 20:43:50 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjWdmTmD9vk9QvVKkHDRwkjqN_BBCIV1g",
+  authDomain: "transport-expenses-a29c5.firebaseapp.com",
+  projectId: "transport-expenses-a29c5",
+  storageBucket: "transport-expenses-a29c5.firebasestorage.app",
+  messagingSenderId: "757801835710",
+  appId: "1:757801835710:web:e80b2e0eb2ce1731ab12ef"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
